1db42e51741c2735df8a9ffd61b4d03cea8a1ad5
A-Salty-Fish <1309032417@qq.com>
2020-08-07 16:25:46 +0800

    改用el-table“

--- src/views/Notice/index.vue
+++ src/views/Notice/index.vue
@@ -1,30 +1,75 @@
 <template>
   <div>
-    <el-divider></el-divider>
-    <notice v-for="(title, index) in titles" :key="index" v-bind:notice-content="contents[index]" v-bind:notice-title="title"></notice>
-    <el-pagination
-          layout="prev, pager, next"
-          :total="50">
-        </el-pagination>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        align='center'
+        prop="date"
+        label="日期"
+        min-width="360px">
+      </el-table-column>
+      <el-table-column
+        align='center'
+        prop="title"
+        label="标题"
+        min-width="360px">
+      </el-table-column>
+      <el-table-column
+        align='center'
+        label="操作"
+        min-width="360px">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="pagination">
+      <el-pagination
+        :current-page="currentPage"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="10"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="tableData.length"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import Notice from './notice'
 export default {
   name: 'Index',
   data() {
     return {
-      titles: ['标题1', '标题2', '标题3', '标题4'],
-      contents: ['内容1', '内容2', '内容3', '内容4']
+      currentPage: 1,
+      tableData: []
     }
   },
-  components: { Notice }
+  created() {
+    var that = this
+    for (var i = 0; i < 41; i++) {
+      that.tableData[i] = {
+        date: '日期' + i,
+        title: '标题' + i
+      }
+    }
+  },
+  methods: {
+    handleSizeChange() {
+    },
+    handleCurrentChange() {
+      console.log(this.currentPage)
+    }
+  }
 }
 </script>
 
 <style scoped>
-  .el-pagination {
+  .el-pagination{
     text-align: center;
+    margin-top: 5%;
   }
 </style>
