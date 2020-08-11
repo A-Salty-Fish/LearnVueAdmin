9325d95e4cc6e7acf38b5051878ec0fd31cd0084
A-Salty-Fish <1309032417@qq.com>
2020-08-11 14:13:51 +0800

    饼图

--- src/views/charts/index.vue
+++ src/views/charts/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="Echarts">
-    <div id="main" style="width: 600px;height:400px;" />
+    <div id="chart1" style="width: 600px;height:600px; display: flex" />
+    <div id="chart2" style="width: 600px;height:600px; display: flex" />
   </div>
 </template>
 
@@ -8,35 +9,68 @@
 import echarts from 'echarts'
 export default {
   mounted() {
-    this.myEcharts()
+    this.myEcharts1()
+    this.myEcharts2()
   },
   methods: {
-    myEcharts() {
-      // 基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(document.getElementById('main'))
+    myEcharts1() {
+      var pieEchart = echarts.init(document.getElementById('chart1'))
 
       // 指定图表的配置项和数据
-      var option = {
+      var pieoption = {
         title: {
-          text: 'ECharts 入门示例'
+          text: '签到情况',
+          x: 'left',
+          textStyle: {
+            color: 'black',
+            fontSize: 25
+          }
         },
-        tooltip: {},
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} KM'
+        },
         legend: {
-          data: ['销量']
-        },
-        xAxis: {
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-        },
-        yAxis: {},
-        series: [{
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }]
-      }
+          orient: 'vertical',
+          x: 'left',
+          top: 40,
+          itemWidth: 70,
+          itemHeight: 30,
+          formatter: '{name}',
+          textStyle: {
+            color: 'black'
+          },
+          data: [{ name: '区域外', icon: 'rect' }, { name: '区域内', icon: 'rect' }] },
+        calculable: true,
+        series: [
+          {
+            name: '签到情况',
+            type: 'pie',
+            radius: '50%', // 饼图的半径大小
+            center: ['50%', '50%'], // 饼图的位置
+            label: { // 饼图图形上的文本标签
+              normal: {
+                show: true,
+                position: 'inner', // 标签的位置
+                textStyle: {
+                  fontWeight: 300,
+                  fontSize: 16 // 文字的字体大小
+                },
+                formatter: '{d}%'
 
+              }
+            },
+            data: [
+              { value: 50, name: '区域外', itemStyle: { normal: { color: 'red' }}},
+              { value: 236, name: '区域内', itemStyle: { normal: { color: 'green' }}}
+            ]
+          }
+        ]
+      }
       // 使用刚指定的配置项和数据显示图表。
-      myChart.setOption(option)
+      pieEchart.setOption(pieoption)
+    },
+    myEcharts2() {
     }
   }
 }
