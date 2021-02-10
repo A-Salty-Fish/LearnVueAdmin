a2fc1215fd49521d531b5d0d0e3aae72737ddb5f
A-Salty-Fish <1309032417@qq.com>
2021-02-10 21:50:32 +0800

    显示信息

--- src/views/TencentMap/Map.vue
+++ src/views/TencentMap/Map.vue
@@ -6,7 +6,7 @@
 
 <script>
 var center = new TMap.LatLng(30.538435, 114.357496)
-var map, markerLayer
+var map, markerLayer, markerCluster, infoWindow
 import { signInAPI } from '@/api/signin.js'
 export default {
   name: 'Index',
@@ -25,9 +25,6 @@ export default {
     this.setMapData()
   },
   created() {
-    signInAPI().getToday().then(res => {
-      console.log(res.data)
-    })
   },
   methods: {
     setMapData() {
@@ -81,44 +78,63 @@ export default {
 
       // 监听回调函数（非匿名函数）
       var clickHandler = function(evt) {
-        // console.log(evt.geometries[0].properties.name)
-        evt.cluster.geometries.forEach(x => console.log(x.properties.name))
-        // console.log(evt.cluster.geometries[0].properties.name)
-        // alert(evt.geometry)
+        evt.cluster.geometries.forEach(x => console.log(x.properties))
       }
+      infoWindow = new TMap.InfoWindow({
+        map: map,
+        position: new TMap.LatLng(30.538435, 114.357496),
+        offset: { x: 0, y: -32 } // 设置信息窗相对position偏移像素，为了使其显示在Marker的上方
+      })
+      infoWindow.close()// 初始关闭信息窗关闭
       // 监听marker点击事件
       // markerLayer.on('click', clickHandler)
-
-      // 创建点聚合对象
-      var markerCluster = new TMap.MarkerCluster({
-        id: 'cluster', // 图层id
-        map: map, // 设置点聚合显示在哪个map对象中（创建map的段落省略）
-        enableDefaultStyle: true, // 使用默认样式
-        minimumClusterSize: 2, // 最小聚合点数：2个
-        geometries: [
-          {
-            position: new TMap.LatLng(30.538325, 114.357496),
-            properties: { name: 'dzy' }
-          },
-          { position: new TMap.LatLng(30.538355, 114.357486),
-            properties: { name: 'dzy2' }
-          },
-          { position: new TMap.LatLng(30.538335, 114.357476),
-            properties: { name: 'dzy3' }
-          },
-          { position: new TMap.LatLng(30.538305, 114.357466),
-            properties: { name: 'dzy4' }
-          }
-          // ....将您所有要打到图中的坐标点传入进来
-        ],
-        zoomOnClick: true, // 点击聚合数字放大展开
-        gridSize: 30, // 聚合算法的可聚合距离，即距离小于该值的点会聚合至一起，默认为60，以像素为单位
-        averageCenter: true, // 每个聚和簇的中心是否应该是聚类中所有标记的平均值
-        maxZoom: 18 // 采用聚合策略的最大缩放级别，若地图缩放级别大于该值，则不进行聚合，标点将全部被展开
+      signInAPI().getToday().then(res => {
+        // 创建点聚合对象
+        markerCluster = new TMap.MarkerCluster({
+          id: 'cluster', // 图层id
+          map: map, // 设置点聚合显示在哪个map对象中（创建map的段落省略）
+          enableDefaultStyle: true, // 使用默认样式
+          minimumClusterSize: 2, // 最小聚合点数：2个
+          geometries: [
+            // ....将您所有要打到图中的坐标点传入进来
+          ],
+          zoomOnClick: false, // 点击聚合数字放大展开
+          gridSize: 30, // 聚合算法的可聚合距离，即距离小于该值的点会聚合至一起，默认为60，以像素为单位
+          averageCenter: true, // 每个聚和簇的中心是否应该是聚类中所有标记的平均值
+          maxZoom: 18 // 采用聚合策略的最大缩放级别，若地图缩放级别大于该值，则不进行聚合，标点将全部被展开
+        })
+        res.data.forEach(x => {
+          markerCluster.add({
+            position: new TMap.LatLng(x.latitude, x.longitude),
+            properties: x
+          })
+        })
+        markerCluster.on('click', function(evt) {
+          // 设置infoWindow
+          infoWindow.open() // 打开信息窗
+          console.log(evt)
+          infoWindow.setPosition(evt.cluster.center)// 设置信息窗位置
+          var conStr = '<table>\n' +
+            '<tr>\n' +
+            '<td>姓名</td>\n' +
+            '<td>学号</td>\n' +
+            '<td>经纬度</td>\n' +
+            '<td>时间</td>\n' +
+            '</tr>\n'
+          evt.cluster.geometries.forEach(x => {
+            var xp = x.properties
+            conStr += `<tr>\n` +
+              `<td>${xp.name}</td>\n` +
+              `<td>${xp.number}</td>\n` +
+              `<td>(${xp.latitude},${xp.longitude})</td>\n` +
+              `<td>${xp.date}</td>\n` +
+              `</tr>\n`
+          })
+          conStr += '</table>'
+          infoWindow.setContent(conStr)// 设置信息窗内容
+        })
       })
-      markerCluster.on('click', clickHandler)
     }
-
   }
 }
 </script>
